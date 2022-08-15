00a85785373e1c38a1a2e1d6af3c38a1a12059e3
Marius Aalto <108743532+aaltofar@users.noreply.github.com>
2022-08-15 15:32:34 +0200

    Dag 1

---
+++ script.js
@@ -0,0 +1,41 @@
+
+function openLars(){
+    document.getElementById('lars').innerHTML = /*HTML*/`
+        <p>
+        
+    `;
+    
+}
+
+
+
+
+function openSimen() {
+    document.getElementById("simen").innerHTML = /*HTML*/ `
+    <div class="box" id="simen">
+          <p> Simen er en råtass på 10m sofa med backflip </p>
+    <video class="simenvideo" controls controlsList="nodownload noaudio" preload="none">
+                <source src="https://cdn.discordapp.com/attachments/1005033299334398115/1008720444725141675/trim.2F15BF93-DFAE-4184-8BD0-7A28CFFDFDD6.mov" type="video/mp4">
+            </video>  
+
+        </div>
+    `;
+}
+
+function openJoakim(){
+    document.getElementById('joakim').innerHTML = /*HTML*/`
+    <p>
+        Joakim er en linguistisk trylleknustner. </br> 
+        Han kan faktisk snakke flytende svensk!
+    </p>
+    `;
+}
+
+function openMarius(){
+    document.getElementById('marius').innerHTML = /*HTML*/`
+
+    <p>
+        
+    
+    </p>
+}
