4b6bae7704d447d5c042fb2a37f1a758b2ff37f3
Marius Aalto <108743532+aaltofar@users.noreply.github.com>
2022-08-16 15:11:47 +0200

    dag 2

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+
+
 function openLars(){
     document.getElementById('lars').innerHTML = /*HTML*/`
         <p>
@@ -7,35 +9,34 @@ function openLars(){
     
 }
 
-
-
-
 function openSimen() {
     document.getElementById("simen").innerHTML = /*HTML*/ `
-    <div class="box" id="simen">
+    <div>
           <p> Simen er en råtass på 10m sofa med backflip </p>
     <video class="simenvideo" controls controlsList="nodownload noaudio" preload="none">
                 <source src="https://cdn.discordapp.com/attachments/1005033299334398115/1008720444725141675/trim.2F15BF93-DFAE-4184-8BD0-7A28CFFDFDD6.mov" type="video/mp4">
             </video>  
-
         </div>
     `;
 }
 
-function openJoakim(){
+function openJoakim() {
     document.getElementById('joakim').innerHTML = /*HTML*/`
-    <p>
-        Joakim er en linguistisk trylleknustner. </br> 
-        Han kan faktisk snakke flytende svensk!
-    </p>
+        <div>
+            Joakim er en linguistisk trylleknustner. </br> 
+            Han kan faktisk snakke flytende svensk!
+        </div>
     `;
+    console.log('funker');
 }
 
-function openMarius(){
+function openMarius() {
     document.getElementById('marius').innerHTML = /*HTML*/`
-
-    <p>
-        
-    
-    </p>
+        <div>
+            <p>
+                marius fikk vondt i toa
+            
+            </p>
+        </div>
+    `;
 }
